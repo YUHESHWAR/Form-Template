db7ac7f42cf65393c942eff9ca17dec2306766be
Yuheshwar K S <91623509+YUHESHWAR@users.noreply.github.com>
2022-08-19 21:57:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let data ={};
+var form_ele = document.querySelector('form');
+form_ele.addEventListener('submit',(e)=>{
+    var inputs = e.target.elements;
+    e.preventDefault();
+    for(var i = 0;i<inputs.length;i++){
+        if( inputs[i].nodeName === "INPUT" && inputs[i].type=='radio')
+            data[inputs[i].name] = (document.querySelector(`input[type=radio][name=${inputs[i].name}]:checked`)).value;
+        else if ( inputs[i].nodeName === "INPUT" || inputs[i].nodeName === "TEXTAREA")
+            data[inputs[i].name] = inputs[i].value;
+    }
+    console.log(data);
+})
+
+//To controll the theme of the form
+var body = document.querySelector('body')
+var themeFlag= true;
+function change(){
+    if(themeFlag){
+        body.classList.replace('theme','white');
+        themeFlag=false;
+    }
+    else{
+        body.classList.replace('white','theme');
+        themeFlag=true;
+    }
+}
+
+
+
